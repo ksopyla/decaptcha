4e806a2dd07b948a0677bd30293435cfb43aed51
ksirg <ksirg@wp.pl>
2016-07-25 16:25:15 +0200

    fix all the print functions, adapt to python 3

--- decaptcha_convnet.py
+++ decaptcha_convnet.py
@@ -202,7 +202,7 @@ with tf.Session() as sess:
             batch_loss = sess.run(loss, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.})
             losses.append(batch_loss)
             
-            print "Iter " + str(step*batch_size) + " started={}".format(dt.datetime.now()) + ", Minibatch Loss= " + "{:.6f}".format(batch_loss) + ", Training Accuracy= " + "{}".format(acc)
+            print("Iter " + str(step*batch_size) + " started={}".format(dt.datetime.now()) + ", Minibatch Loss= " + "{:.6f}".format(batch_loss) + ", Training Accuracy= " + "{}".format(acc))
             
             batch_idx=0
             k=idx[batch_idx]
@@ -220,16 +220,16 @@ with tf.Session() as sess:
             true_word = vecmp.map_vec_pos2words(max_idx_l[batch_idx,:])
 
             
-            print("true : {}, predicted {}".format(true_word, predicted_word)
+            print("true : {}, predicted {}".format(true_word, predicted_word))
 
             epoch+=1
         
         step += 1
         
     end_epoch = dt.datetime.now()
-    print "Optimization Finished, end={} duration={}".format(end_epoch,end_epoch-start_epoch)
+    print("Optimization Finished, end={} duration={}".format(end_epoch,end_epoch-start_epoch))
     # Calculate accuracy 
-    print "Testing Accuracy:", sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.})
+    print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys, keep_prob: 1.}))
     
 
 import matplotlib.pyplot as plt
